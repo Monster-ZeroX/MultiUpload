e6fc71ca914c471981a4f2f05e9f724343ca3336
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-06-29 01:27:58 +0530

    Update tmpninja.py

--- multiupload/plugins/tmpninja.py
+++ multiupload/plugins/tmpninja.py
@@ -1,8 +1,4 @@
-import shlex
-import json
-import subprocess
-
-import asyncio, json, os
+import asyncio, json, os, requests
 from multiupload import anjana
 from telethon.sync import events
 
@@ -17,13 +13,8 @@ async def tmpninja(e):
 	await snd.edit('Success !!\n Path: '+path)
     
 	#await snd.edit('Now uploading to AnonFile')
-	cURL = f"""curl -i -F files[]=@{path} https://tmp.ninja/upload.php"""
-
-	lCmd = shlex.split(cURL) # Splits cURL into an array
-
-	p = subprocess.Popen(lCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	out, err = p.communicate() # Get the output and the err message
-
-	json_data = json.loads(out.decode("utf-8"))
-
-	print(json_data) # Display now the data
+	url = 'https://tmp.ninja/upload.php'
+	payload = open(f"{path}")
+	r = requests.post(url, data=payload)
+	print(r)
+	print(type(r))
